50b6533cf7191b5fa8d8a9a470740dc5e3e21ad1
Dimpu Aravind Buddha <dimpu@users.noreply.github.com>
2023-07-11 20:25:29 -0700

    Update hello.ts

--- hello.ts
+++ hello.ts
@@ -1,6 +1,18 @@
 import { serve } from "https://deno.land/std@0.192.0/http/server.ts";
+
+let port = 8000;
+let content = "Hellow World";
+
 function handler(_req: Request): Response {
-  return new Response("Hello, World!");
+  const portIndex = Deno.args.findIndex(arg => arg === "--port");
+  if(portIndex > -1) {
+    port = Deno.args[portIndex+1];
+  }
+  const contentIndex = Deno.args.findIndex(arg => arg === "--content");
+  if(contentIndex > -1) {
+    content = Deno.args[contentIndex+1];
+  }
+  return new Response(content);
 }
-console.log("Listening on http://localhost:8000");
-serve(handler);
+console.log("Listening on http://localhost:" + port);
+serve({port, handler});
